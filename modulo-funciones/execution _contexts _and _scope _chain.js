4fbe43d9f2499af480148cc6cb6b9f5099e1cb00
Robinson Mendoza <rmendozac@unal.edu.co>
2025-06-01 00:22:24 -0500

    Create 'execution _contexts _and _scope _chain.js'

---
+++ modulo-funciones/execution _contexts _and _scope _chain.js
@@ -0,0 +1,47 @@
+// const productName = "laptop";
+// const price = 899;
+// const brand = "techCode";
+
+// function getProductInfo() {
+//   const productName = "smartphone";
+//   const price = 499;
+//   return `${productName} costs ${price} and is of brand ${brand}`;
+// }
+
+// console.log(getProductInfo());
+
+---
+// let userPoints = 80;
+
+// function evaluatePoints() {
+//   if (userPoints < 100) {
+//       console.log("Below average");
+//   } else {
+//       console.log("Above average");
+//   }
+// }
+
+// console.log(evaluatePoints());
+---
+// const globalVariable = "global1";
+
+// function local1() {
+//   console.log(globalVariable); // Successfully accesses "global1"
+//   console.log(localVariable); // Error: localVariable is undefined
+
+//   function local2() {
+//       const carrot = "🥕";
+//       console.log(`local2: ${carrot}`);
+//   }
+
+//   function local3() {
+//       console.log(local3Variable); // Error here too
+//   }
+
+//   local2();
+//   local3();
+// }
+// console.log(local1());
+---
+
+  
